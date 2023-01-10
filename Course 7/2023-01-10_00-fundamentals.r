6f8d8e13dd0c9b4b8d2e2b33d0b7804a230298db
aidanlokeeffe <42976464+aidanlokeeffe@users.noreply.github.com>
2023-01-10 17:51:50 -0600

    Work for 2023-01-10
    Finished course 6, beginning course 7

---
+++ Course 7/2023-01-10_00-fundamentals.r
@@ -0,0 +1,16 @@
+print("Coding in R")
+
+?print()
+?Print()
+
+# Here's an example of a variable
+first_variable <- "This is my variable"
+second_variable <- 12.5
+
+# Vectors
+vec_1 <- c(13, 48.5, 71, 101.5,2)
+vec_1
+
+# Pipes: a tool in R for expressing a sequence of multiple
+# operations, represented with "%>%"
+# We'll use these more later
---
+++ Course 7/2023-01-10_01-operators.R
@@ -0,0 +1,9 @@
+# Our first calculations
+quarter_1_sales <- 35657.98
+quarter_2_sales <- 43810.55
+midyear_sales <- quarter_1_sales + quarter_2_sales
+midyear_sales
+
+# One more calculation
+yearend_sales = 2*midyear_sales
+yearend_sales
